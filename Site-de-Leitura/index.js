29ce43c0f761d5517e2090f3aa3903dcd61564d2
LucasMarani <lucas.marani@edu.unifil.br>
2025-03-31 19:32:49 -0300

    Versão final

---
+++ Site-de-Leitura/index.js
@@ -0,0 +1,175 @@
+function SelectResume(name){
+
+    if(name == 'l1'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro1.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Eu Robô \nAutor: Isaac Asimov';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/Eu_Robô-Isaac_Asimov.pdf');
+        document.getElementById('textResume').innerText = 'Um dos maiores clássicos da literatura de ficção científica, Eu, Robô, escrito por Isaac Asimov, é um conjunto de nove contos, interconectados pela Dra. Susan Calvin, que apresenta seus relatos sobre a evolução dos autômatos através do tempo. É nesta obra que Asimov apresentada as célebres Três Leis da Robótica, princípios que regem o comportamento dos autômatos em toda a sua trajetória.'
+    }
+    if(name == 'l2'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro2.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: O Galinheiro \nAutor: Amauricio Lopes';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/O-Galinheiro-Amauricio-Lopes.pdf');
+        document.getElementById('textResume').innerText = 'Uma vez por ano, no mesmo feriado, Maria Neide da Silva e Francisco roubam algum galinheiro da cidade de Palheiros com os filhos. Movidos por uma suspeita quanto ao paradeiro da filha do meio, desaparecida há seis meses, invadem o quintal da maior e mais imponente casa da cidade, a única que fica do outro lado do rio. Dentro da propriedade, precisam tomar cuidado para não ser percebidos. E medo e coragem se alternam numa luta desesperada não apenas pela verdade, mas também pela vida.'
+    }
+    if(name == 'l3'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro3.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Hype \n Autora: Gabrielle Bluestone';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/Hype-Gabrielle Bluestone.pdf');
+            document.getElementById('textResume').innerText = 'Baseando-se em pesquisas científicas, campanhas de marketing, documentos e entrevistas exclusivas, Gabrielle Bluestone mergulha no hype irresistível que alimenta o ecossistema de nossas redes sociais, seja ele vindo de influenciadores confiáveis que venderam o Fyre Festival ou das avaliações de consumidores que venderam o Juicero. Um exame cultural que é tão revelador quanto relevante, Hype esclarece o jogo de manipulação por trás da temporada eterna de golpes ― e como nós, enquanto consumidores, somos passados para trás.'
+    }
+    if(name == 'l4'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro4.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Dez dias num Hospício \nAutor: Nellie Bly';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/Dez_Dias_Em_um_Hospicio-Nellie_Bly.pdf');
+            document.getElementById('textResume').innerText = 'Nellie Bly foi uma corajosa jornalista que aceitou o desafiador trabalho de internar-se em um hospital psiquiátrico. Seu objetivo era entender e relatar as torturas e descasos que ocorriam entre enfermeiros, médicos e pacientes, assim como o processo de triagem das próximas internações. Bly descreve que nem todos os pacientes realmente precisavam de cuidados mentais hospitalares, mas todos, sem exceção, eram tratados de forma desumana. A saída? Provar-se mentalmente são. Uma tarefa difícil para qualquer pessoa emocionalmente abalada pelo medo. Este não é um livro de ficção e contém relatos de violência, opressão, abuso físico e mental.'
+    }
+    if(name == 'l5'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro5.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Isso Nunca vai Funcionar \nAutor: Marc Randolph';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/Isso Nunca vai Funcionar - Marc Randolph.pdf');
+            document.getElementById('textResume').innerText = 'CONHEÇA A INCRÍVEL HISTÓRIA DE COMO A NETFLIX PASSOU DE UMA IDEIA A UMA EMPRESA, NARRADA POR SEU PRIMEIRO CEO Era uma vez um tempo no qual as locadoras de vídeo tradicionais eram as rainhas do pedaço, com fitas VHS que tinham de ser rebobinadas e que rendiam multas se não devolvidas na data estipulada. As multas por atraso eram onipresentes, a transmissão de vídeo inédita e a adoção generalizada do DVD parecia tão iminente quanto carros voadores. Foi neste momento perdido na história, em 1997, que Marc Randolph teve uma ideia. E se usassem a internet para alugar filmes? Não era a primeira ideia do gênero. Ao caminho do trabalho, com o parceiro de negócios Reed Hastings, Marc já tinha falado sobre um serviço de entregas de xampu e um site para desenvolver tacos de beisebol personalizados, entre outras "heurecas". Mas desta vez Hastings ficou intrigado, e a dupla – Hastings como o principal investidor e Randolph como o primeiro CEO – fundou uma empresa. Com mais de 150 milhões de assinantes, o triunfo da Netflix parece inevitável, mas tudo começou com pouca fé e muitos percalços pelo caminho. Essa incrível jornada é um exemplo de como qualquer pessoa com coragem, instinto e determinação pode mudar o mundo – mesmo que muitos achem que sua ideia nunca irá funcionar.'
+    }
+    if(name == 'l6'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro6.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Jogo dos Ladrões \nAutor: Kayvion Lewis';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/Jogo dos Ladrões - Kayvion Lewis.pdf');
+            document.getElementById('textResume').innerText = 'Há gerações a família Quest lidera o mercado de roubos milionários, mas Rosalyn Quest está cansada de se sentir solitária entre uma missão e outra. A jovem quer ir para a faculdade e fazer amigos como uma adolescente comum, e por isso trama um plano para fugir de casa. Porém, no dia da fuga, sua mãe é sequestrada.\n\nDesesperada, a garota procura ajuda para pagar o resgate, mas nada dá certo: o valor é alto demais e ninguém quer se envolver. Ou ela arranja o dinheiro, ou a mãe morre. Correndo contra o tempo e sem alternativa, Ross aceita entrar para o Jogo dos Ladrões, uma competição perigosa e mortal que premia o melhor ladrão de elite com um desejo ― que pode ser qualquer coisa no mundo.\n\nRoss não vai medir esforços para ganhar o jogo e salvar a vida da mãe. Só que as coisas ficam ainda mais complicadas quando, entre os adversários, estão sua arqui-inimiga de infância, Noelia Boschert, e Devroe Kenzie, um garoto charmoso que mexe com seu coração.'
+    }
+    if(name == 'l7'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro7.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Harry Potter e o \n Prisioneiro de Azkaban \nAutor: J. K. Rowling';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/j-k-rowling-3-harry-potter-e-o-prisioneiro-de-azkaban.pdf');
+            document.getElementById('textResume').innerText = 'Depois de Harry Potter e a Pedra Filosofal e Harry Potter e a Câmara Secreta, Harry Potter e o prisioneiro de Azkaban, o terceiro volume da série do bruxo mais adorado do mundo, ganha edição ilustrada pela Rocco. Com projeto gráfico sofisticado - capa dura, sobrecapa e miolo em papel cuchê - o livro traz o texto integral de J.K. Rowling acompanhado dos incríveis desenhos do premiado ilustrador britânico Jim Kay, escolhido para recriar em imagens os sete livros da série. Novamente, Kay apresenta um mundo encantado e repleto de detalhes a cada página, agora para acompanhar o terceiro ano de Harry Potter na Escola de Magia de Hogwarts.'
+    }
+    if(name == 'l8'){
+        var capa = document.getElementById('capaLivro');
+        capa.src = '../img/livro8.webp';
+        document.getElementById('nomeLivro').innerText = 'Título: Harry Potter e a \n Camara Secreta \nAutor: J. K. Rowling';
+        var link = document.getElementById("buttonLer");
+            link.getAttribute('href');
+            link.setAttribute('href','../pdf/j-k-rowling-2-harry-potter-e-a-cc3a2mara-secreta-revisado.pdf');
+            document.getElementById('textResume').innerText = 'Depois de férias aborrecidas na casa dos tios trouxas, está na hora de Harry Potter voltar a estudar. Coisas acontecem, no entanto, para dificultar o regresso de Harry. Persistente e astuto, nosso herói não se deixa intimidar pelos obstáculos e, com a ajuda dos fiéis amigos Weasley, começa o ano letivo na Escola de Magia e Bruxaria de Hogwarts. As novidades não são poucas. Novos colegas, novos professores, muitas e boas descobertas e...um grande e perigosos desafio. Alguém ou alguma coisa ameaça a segurança e a tranqüilidade dos membros de Hogwarts. Como eliminar definitivamente esse mal e restaurar a paz na escola?'
+    }
+    openSlideMenu();
+}
+
+function selectPoema(name){
+
+    if(name == 'p1'){
+        document.getElementById('nomePoema').innerText = 'Soneto de Fidelidade (Vinicius de Moraes)';
+        document.getElementById('textPoema').innerText = 'De tudo, ao meu amor serei atento \nAntes, e com tal zelo, e sempre, e tanto \nQue mesmo em face do maior encanto \nDele se encante mais meu pensamento. \n\nQuero vivê-lo em cada vão momento \nE em louvor hei de espalhar meu canto \nE rir meu riso e derramar meu pranto \nAo seu pesar ou seu contentamento.\n\nE assim, quando mais tarde me procure \nQuem sabe a morte, angústia de quem vive \nQuem sabe a solidão, fim de quem ama\n\nEu possa me dizer do amor (que tive): \nQue não seja imortal, posto que é chama \nMas que seja infinito enquanto dure.'
+    }
+
+    if(name == 'p2'){
+        document.getElementById('nomePoema').innerText = 'Ora direis ouvir estrelas (Olavo Bilac)';
+        document.getElementById('textPoema').innerText = 'Ora (direis) ouvir estrelas! Certo\nPerdeste o senso!” E eu vos direi, no entanto,\nQue, para ouvi-las, muita vez desperto\nE abro as janelas, pálido de espanto…\n\nE conversamos toda a noite, enquanto\nA Via Láctea, como um pálio aberto,\nCintila. E, ao vir do sol, saudoso e em pranto,\nInda as procuro pelo céu deserto.\n\nDireis agora: “Tresloucado amigo!\nQue conversas com elas? Que sentido\nTem o que dizem, quando estão contigo?”\n\nE eu vos direi: “Amai para entendê-las!\nPois só quem ama pode ter ouvido\nCapaz de ouvir e de entender estrelas.”';
+    }
+
+    if(name == 'p3'){
+        document.getElementById('nomePoema').innerText = 'Canção do Exílio (Gonçalves Dias)';
+        document.getElementById('textPoema').innerText = 'Minha terra tem palmeiras,\nOnde canta o Sabiá;\nAs aves, que aqui gorjeiam,\nNão gorjeiam como lá.\n\nNosso céu tem mais estrelas,\nNossas várzeas têm mais flores,\nNossos bosques têm mais vida,\nNossa vida mais amores.\n\nEm cismar, sozinho, à noite,\nMais prazer encontro eu lá;\nMinha terra tem palmeiras,\nOnde canta o Sabiá.\n\nMinha terra tem primores,\nQue tais não encontro eu cá;\nEm cismar — sozinho, à noite —\nMais prazer encontro eu lá;\nMinha terra tem palmeiras,\nOnde canta o Sabiá.\n\nNão permita Deus que eu morra,\nSem que eu volte para lá;\nSem que desfrute os primores\nQue não encontro por cá;\nSem qu’inda aviste as palmeiras,\nOnde canta o Sabiá.';
+    }
+
+    if(name == 'p4'){
+        document.getElementById('nomePoema').innerText = 'Timidez (Cecília Meireles)';
+        document.getElementById('textPoema').innerText = 'Basta-me um pequeno gesto,\nfeito de longe e de leve,\npara que venhas comigo\ne eu para sempre te leve…– mas só esse eu não farei.\n\nUma palavra caída\ndas montanhas dos instantes\ndesmancha todos os mares\ne une as terras mais distantes…– palavra que não direi.\n\nPara que tu me adivinhes,\nentre os ventos taciturnos,\napago meus pensamentos,\nponho vestidos noturnos,– que amargamente inventei.\n\nE, enquanto não me descobres,\nos mundos vão navegando\nnos ares certos do tempo,\naté não se sabe quando…– e um dia me acabarei.';
+    }
+    openSlideMenu2();
+}
+
+
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '1500px';
+    document.getElementById('side-menu').style.marginLeft = '0px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0px';
+    document.getElementById('side-menu').style.marginLeft = '0px';
+}
+
+
+function openSlideMenu2() {
+    document.getElementById('side-menu2').style.width = '700px';
+    document.getElementById('side-menu2').style.height = '550px';
+    document.getElementById('side-menu2').style.marginLeft = '500px';
+    document.getElementById('side-menu2').style.marginTop = '40px';
+}
+
+function closeSlideMenu2() {
+    document.getElementById('side-menu2').style.width = '0px';
+    document.getElementById('side-menu2').style.height = '0px';
+    document.getElementById('side-menu2').style.marginLeft = '0px';
+}
+
+function selecionarFrases(num){
+    var frase = document.getElementById('frase');
+    var posicaoAtual = document.getElementById('posicaoAtual').getAttribute("name");
+
+    posicaoAtual = Number(posicaoAtual) + Number(num); 
+
+    if(posicaoAtual > 9){
+        posicaoAtual = 0;
+    }
+
+    if(posicaoAtual < 0){
+        posicaoAtual = 9;
+    }
+ 
+    if(posicaoAtual == 0){
+        frase.innerText = 'O universo não seria grande coisa se não fosse o lar das pessoas que você ama.'
+    }
+    if(posicaoAtual == 1){
+        frase.innerText = 'Inteligência é a capacidade de se adaptar à mudança.'
+    }
+    if(posicaoAtual == 2){
+        frase.innerText = 'Agressão, por parte dos humanos, pode destruí-los.'
+    }
+    if(posicaoAtual == 3){
+        frase.innerText = 'Eu sou apenas uma criança que nunca cresceu. Eu ainda continuo fazendo aquelas perguntas "como" e "por quê".'
+    }
+    if(posicaoAtual == 4){
+        frase.innerText = 'Embora eu não possa me mover e tenha que falar através de um computador, em minha mente eu sou livre.'
+    }
+    if(posicaoAtual == 5){
+        frase.innerText = 'A vida seria trágica se não fosse engraçada.'
+    }
+    if(posicaoAtual == 6){
+        frase.innerText = 'Meu objetivo é simples: é a compreensão completa do universo, por que ele é como é e por que ele existe.'
+    }
+    if(posicaoAtual == 7){
+        frase.innerText = 'A ciência pode responder o "como", mas não o "porquê".'
+    }
+    if(posicaoAtual == 8){
+        frase.innerText = 'O maior inimigo do conhecimento não é a ignorância, mas a ilusão do conhecimento.'
+    }
+    if(posicaoAtual == 9){
+        frase.innerText = 'A humanidade precisa continuar no espaço para sua sobrevivência a longo prazo.'
+    }
+
+    document.getElementById('posicaoAtual').setAttribute("name",posicaoAtual);
+
+}
+
